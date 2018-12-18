619efd1c473b4fb9882a2ecfbbe1e16ade61c427
OthmaneAmdaui <44850226+OthmaneAmdaui@users.noreply.github.com>
2018-12-18 08:53:42 +0100

    image

---
+++ race/img/puple_car.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(puple_car)
+
+//======================================================================
+//
+//	puple_car, 16x16@8, 
+//	+ palette 256 entries, not compressed
+//	+ 4 tiles Metatiled by 2x2 not compressed
+//	Total size: 512 + 256 = 768
+//
+//	Time-stamp: 2018-12-17, 09:59:23
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_PUPLE_CAR_H
+#define GRIT_PUPLE_CAR_H
+
+#define puple_carTilesLen 256
+extern const unsigned short puple_carTiles[128];
+
+#define puple_carPalLen 512
+extern const unsigned short puple_carPal[256];
+
+#endif // GRIT_PUPLE_CAR_H
+
+//}}BLOCK(puple_car)
---
+++ race/img/turquoise_car.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(turquoise_car)
+
+//======================================================================
+//
+//	turquoise_car, 16x16@8, 
+//	+ palette 256 entries, not compressed
+//	+ 4 tiles Metatiled by 2x2 not compressed
+//	Total size: 512 + 256 = 768
+//
+//	Time-stamp: 2018-12-17, 10:04:20
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_TURQUOISE_CAR_H
+#define GRIT_TURQUOISE_CAR_H
+
+#define turquoise_carTilesLen 256
+extern const unsigned short turquoise_carTiles[128];
+
+#define turquoise_carPalLen 512
+extern const unsigned short turquoise_carPal[256];
+
+#endif // GRIT_TURQUOISE_CAR_H
+
+//}}BLOCK(turquoise_car)
